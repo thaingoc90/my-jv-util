b6fd4429ebb2ba5c1ead0720688166035d7e710a
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-07-24 11:22:00 +0000

    - implement cache

---
+++ trunk/UtilsLibrary/src/my/library/cache/CacheConstant.java
@@ -0,0 +1,10 @@
+package my.library.cache;
+
+public class CacheConstant {
+
+	public final static long DEFAULT_CACHE_CAPACITY = 1000l;
+	public final static long DEFAULT_EXPIRE_AFTER_WRITE = 1800l;
+	public final static long DEFAULT_EXPIRE_AFTER_ACCESS = 1800l;
+
+	public final static int DEFAULT_REDIS_MAX_ACTIVE = 200;
+}
---
+++ trunk/UtilsLibrary/src/my/library/cache/ICache.java
@@ -0,0 +1,120 @@
+package my.library.cache;
+
+import java.util.List;
+
+public interface ICache {
+
+	/**
+	 * Initializing method.
+	 */
+	public void init();
+
+	/**
+	 * Destruction method.
+	 */
+	public void destroy();
+
+	/**
+	 * Gets cache's name
+	 * 
+	 * @return
+	 */
+	public String getName();
+
+	/**
+	 * Gets cache's size
+	 * 
+	 * @return
+	 */
+	public long getSize();
+
+	/**
+	 * Gets cache's number of hits
+	 * 
+	 * @return
+	 */
+	public long getHits();
+
+	/**
+	 * Gets cache's number of misses
+	 * 
+	 * @return
+	 */
+	public long getMisses();
+
+	/**
+	 * Get cache's capacity (the max number of entries)
+	 * 
+	 * @return
+	 */
+	public long getCapacity();
+
+	/**
+	 * Gets number of seconds before entries to be expired since the last access
+	 * 
+	 * @return
+	 */
+	public long getExpireAfterAccess();
+
+	/**
+	 * Gets number of seconds before entries to be expired since the last write
+	 * 
+	 * @return
+	 */
+	public long getExpireAfterWrite();
+
+	/**
+	 * Puts an entry to cache, default expiry
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void set(String key, Object value);
+
+	/**
+	 * Puts an entry to cache, with expireAfterAccess or expireAfterWrite
+	 * 
+	 * @param key
+	 * @param value
+	 * @param expireAfterAcess
+	 * @param expireAfterWrite
+	 */
+	// public void set(String key, Object value, long expireAfterAccess, long
+	// expireAfterWrite);
+
+	/**
+	 * Gets an entry from cache by key
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public Object get(String key);
+
+	/**
+	 * Deletes an entry from cache
+	 * 
+	 * @param key
+	 */
+	public void delete(String key);
+
+	/**
+	 * Clears all entries in cache
+	 */
+	public void clear();
+
+	/**
+	 * Checks if an entry exists in cache
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public boolean exists(String key);
+
+	/**
+	 * Gets all keys from cache which match with pattern
+	 * 
+	 * @param pattern
+	 * @return
+	 */
+	public List<String> searchKey(String pattern);
+}
