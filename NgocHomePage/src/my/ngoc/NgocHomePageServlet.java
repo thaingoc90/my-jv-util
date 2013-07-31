cabb07ad9da94b738dbcd002616f8f917b86ec1e
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-07-31 08:23:50 +0000

    First step

---
+++ NgocHomePage/src/my/ngoc/NgocHomePageServlet.java
@@ -0,0 +1,13 @@
+package my.ngoc;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class NgocHomePageServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
