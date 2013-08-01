0d523553773c8196867b6ade5d49eafca363b289
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-08-01 10:40:58 +0000

    -Test Controller

--- NgocHomePage/src/ind/web/nhp/controller/HomeController.java
+++ NgocHomePage/src/ind/web/nhp/controller/HomeController.java
@@ -1,16 +1,17 @@
 package ind.web.nhp.controller;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@SuppressWarnings("serial")
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class HomeController extends BaseController {
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, this is Spinning's page");
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String get(HttpServletRequest req, HttpServletResponse resp) {
+		return "home";
 	}
 }
