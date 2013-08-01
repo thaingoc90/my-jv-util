53af28644d0f77677ca192c4c5c1e2701b793e56
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-08-01 10:40:58 +0000

    -Test Controller

--- trunk/NgocHomePage/src/ind/web/nhp/controller/BaseController.java
+++ trunk/NgocHomePage/src/ind/web/nhp/controller/BaseController.java
@@ -1,8 +1,5 @@
 package ind.web.nhp.controller;
 
-import javax.servlet.http.HttpServlet;
-
-@SuppressWarnings("serial")
-public abstract class BaseController extends HttpServlet {
+public abstract class BaseController {
 
 }
