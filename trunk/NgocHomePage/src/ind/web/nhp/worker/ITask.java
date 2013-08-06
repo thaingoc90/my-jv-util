deeac09b60aa9b1e2a64d71e3ec0294ba08afb79
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-08-06 11:13:32 +0000

    -jsoup & base worker

---
+++ trunk/NgocHomePage/src/ind/web/nhp/worker/ITask.java
@@ -0,0 +1,30 @@
+package ind.web.nhp.worker;
+
+public interface ITask {
+
+	public static enum Status {
+		RUNNING, // task is running
+		IDLE, // task is idle
+	};
+
+	public static enum Scheduling {
+		RUNONCE, // task runs only once
+		CONTINUOUS, // task runs continuously (ignore fixed rate delay)
+		REPEATED, // task runs repeatedly at a fixed rate
+	};
+
+	/**
+	 * Gets task's unique id.
+	 * 
+	 * @return
+	 */
+	public String getId();
+
+	/**
+	 * Executes task.
+	 * 
+	 * @param params
+	 * @return
+	 */
+	public Object executeTask(Object params);
+}
