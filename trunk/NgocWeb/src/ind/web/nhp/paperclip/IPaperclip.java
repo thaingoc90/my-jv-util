b7dba7562270c690c1b62d3c99f4e8f4ac9a8242
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-08-22 10:43:21 +0000

    paperclip

---
+++ trunk/NgocWeb/src/ind/web/nhp/paperclip/IPaperclip.java
@@ -0,0 +1,31 @@
+package ind.web.nhp.paperclip;
+
+import java.util.Date;
+
+public interface IPaperclip {
+
+    public String getId();
+
+    public String getOriginalName();
+
+    public String getStatus();
+
+    public String getType();
+
+    public String getOwner();
+
+    public Date getCreateTime();
+
+    public int getSize();
+
+    public String getMeta();
+
+    public String getDiskName();
+
+    public String getDir();
+
+    public boolean isExternalStorage();
+
+    public String getUrl(String prefix);
+
+}
---
+++ trunk/NgocWeb/src/ind/web/nhp/paperclip/PaperclipManager.java
@@ -0,0 +1,5 @@
+package ind.web.nhp.paperclip;
+
+public class PaperclipManager {
+
+}
