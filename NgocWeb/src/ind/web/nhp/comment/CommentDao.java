3ee60b0a06be515c46da53cbd7fee74639d537de
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-08-30 10:31:34 +0000

    -comment system (start)

---
+++ NgocWeb/src/ind/web/nhp/comment/CommentDao.java
@@ -0,0 +1,170 @@
+package ind.web.nhp.comment;
+
+import ind.web.nhp.base.BaseJdbcDao;
+import ind.web.nhp.utils.Utils;
+
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommentDao extends BaseJdbcDao implements ICommentDao {
+
+	public final static String FIELD_TARGET_ID = "target_id";
+	public final static String FIELD_CONTENT = "content";
+	public final static String FIELD_TOKEN = "token";
+	public final static String FIELD_ACCOUNT_NAME = "account_name";
+	public final static String FIELD_COMMENT_ID = "comment_id";
+	public final static String FIELD_PARENT_ID = "parent_comment_id";
+	public final static String FIELD_CREATED = "created";
+	public final static String FIELD_UPDATED = "updated";
+	public final static String FIELD_UPDATED_BY = "updated_by";
+	public final static String FIELD_STATUS = "status";
+	public final static String FIELD_APPROVED_BY = "approved_by";
+	public final static String FIELD_TOTAL_LIKES = "total_likes";
+
+	public final static String FIELD_START_INDEX = "start_index";
+	public final static String FIELD_PAGE_SIZE = "page_size";
+
+	private Logger LOGGER = LoggerFactory.getLogger(CommentDao.class);
+
+	@Override
+	public Long addComment(String accountName, String content, Long targetId, String token,
+			Long parentCommentId) {
+		final String sqlKey = "sql.addComment";
+		long commentId = Utils.generateLongId64();
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put(FIELD_COMMENT_ID, commentId);
+		params.put(FIELD_ACCOUNT_NAME, accountName);
+		params.put(FIELD_TARGET_ID, targetId);
+		params.put(FIELD_CONTENT, content);
+		params.put(FIELD_STATUS, 0);
+		params.put(FIELD_TOKEN, token);
+		params.put(FIELD_PARENT_ID, parentCommentId);
+		try {
+			long result = executeNonSelect(sqlKey, params);
+			return (result > 0) ? commentId : null;
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new RuntimeException();
+		} finally {
+		}
+	}
+
+	@Override
+	public boolean deleteComment(Long commentId, Long targetId, String token) {
+		final String sqlKey = "sql.deleteComment";
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put(FIELD_COMMENT_ID, commentId);
+		params.put(FIELD_TARGET_ID, targetId);
+		params.put(FIELD_TOKEN, token);
+		try {
+			long result = executeNonSelect(sqlKey, params);
+			return (result > 0) ? true : false;
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new RuntimeException();
+		} finally {
+		}
+	}
+
+	@Override
+	public Long updateComment(Long commentId, Long targetId, String token, String content,
+			String accountUpdate) {
+		final String sqlKey = "sql.updateComment";
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put(FIELD_COMMENT_ID, commentId);
+		params.put(FIELD_UPDATED_BY, accountUpdate);
+		params.put(FIELD_TARGET_ID, targetId);
+		params.put(FIELD_CONTENT, content);
+		params.put(FIELD_TOKEN, token);
+		try {
+			long result = executeNonSelect(sqlKey, params);
+			return (result > 0) ? commentId : null;
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new RuntimeException();
+		} finally {
+		}
+	}
+
+	@Override
+	public Long approveComment(Long commentId, Long targetId, String token, String approver,
+			Integer status) {
+		final String sqlKey = "sql.approveComment";
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put(FIELD_COMMENT_ID, commentId);
+		params.put(FIELD_TARGET_ID, targetId);
+		params.put(FIELD_TOKEN, token);
+		params.put(FIELD_APPROVED_BY, approver);
+		params.put(FIELD_UPDATED, new Date());
+		params.put(FIELD_STATUS, status);
+		try {
+			long result = executeNonSelect(sqlKey, params);
+			return (result > 0) ? commentId : null;
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new RuntimeException();
+		} finally {
+		}
+	}
+
+	@Override
+	public Map<String, Object> getComment(Long commentId, Long targetId, String token) {
+		final String sqlKey = "sql.getComment";
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put(FIELD_COMMENT_ID, commentId);
+		params.put(FIELD_TARGET_ID, targetId);
+		params.put(FIELD_TOKEN, token);
+		try {
+			List<Map<String, Object>> cms = executeSelect(sqlKey, params);
+			return cms != null && cms.size() > 0 ? cms.get(0) : null;
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new RuntimeException();
+		} finally {
+		}
+	}
+
+	@Override
+	public List<Map<String, Object>> getChildComments(Long parentCommentId, Long targetId,
+			String token) {
+		final String sqlKey = "sql.getChildComments";
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put(FIELD_PARENT_ID, parentCommentId);
+		params.put(FIELD_TARGET_ID, targetId);
+		params.put(FIELD_TOKEN, token);
+		try {
+			return executeSelect(sqlKey, params);
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new RuntimeException();
+		} finally {
+		}
+	}
+
+	@Override
+	public List<Map<String, Object>> getCommentsByTarget(Long targetId, String token,
+			Integer status, Integer page, Integer pageSize) {
+		final String sqlKey = "sql.getCommentsByTarget";
+		int ignoreStatus = status == null ? 1 : 0;
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put(FIELD_TARGET_ID, targetId);
+		params.put(FIELD_TOKEN, token);
+		params.put("ignore_status", ignoreStatus);
+		params.put(FIELD_STATUS, status);
+		params.put(FIELD_START_INDEX, (page - 1) * pageSize);
+		params.put(FIELD_PAGE_SIZE, pageSize);
+		try {
+			return executeSelect(sqlKey, params);
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new RuntimeException();
+		} finally {
+		}
+	}
+}
--- NgocWeb/src/ind/web/nhp/utils/Utils.java
+++ NgocWeb/src/ind/web/nhp/utils/Utils.java
@@ -1,5 +1,6 @@
 package ind.web.nhp.utils;
 
+import java.util.Map;
 import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
@@ -51,14 +52,33 @@ public class Utils {
 		return session.getAttribute(name);
 	}
 
+	/**
+	 * Validates email pattern
+	 * 
+	 * @param email
+	 * @return
+	 */
 	public static boolean isValidEmail(String email) {
 		return EmailValidator.getInstance().isValid(email);
 	}
 
+	/**
+	 * Converts obj to int-type. If not, return 0.
+	 * 
+	 * @param obj
+	 * @return
+	 */
 	public static int toInt(Object obj) {
 		return toInt(obj, 0);
 	}
 
+	/**
+	 * Converts obj to int-type. If not, return default value.
+	 * 
+	 * @param obj
+	 * @param defaultVal
+	 * @return
+	 */
 	public static Integer toInt(Object obj, Integer defaultVal) {
 		if (obj == null) {
 			return defaultVal;
@@ -66,26 +86,151 @@ public class Utils {
 		Integer result = defaultVal;
 		try {
 			result = Integer.parseInt(obj.toString().trim());
-		} catch (NumberFormatException e) {}
+		} catch (NumberFormatException e) {
+		}
 		return result;
 	}
 
+	/**
+	 * Convert Object to String, if obj is null, return default value.
+	 * 
+	 * @param obj
+	 * @param defaultVal
+	 * @return
+	 */
 	public static String toString(Object obj, String defaultVal) {
 		return obj != null ? obj.toString() : defaultVal;
 	}
 
+	/**
+	 * Convert Object to String, if obj is empty, return default value.
+	 * 
+	 * @param obj
+	 * @param defaultVal
+	 * @return
+	 */
 	public static String toStringNotEmpty(Object obj, String defaultVal) {
 		return obj != null && !StringUtils.isEmpty(obj.toString()) ? obj.toString() : defaultVal;
 	}
 
+	/**
+	 * Gets value from map, generic version.
+	 * 
+	 * @param props
+	 * @param lookupKey
+	 * @param clazz
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T getValue(Map<?, ?> props, String key, Class<T> clazz) {
+		Object value = props.get(key);
+		if (value != null) {
+			if (!clazz.isAssignableFrom(value.getClass())) {
+				if (clazz == String.class) {
+					value = value.toString();
+				} else if (clazz == Short.class || clazz == short.class || clazz == Integer.class
+						|| clazz == int.class) {
+					try {
+						value = Integer.parseInt(value.toString().trim());
+					} catch (NumberFormatException e) {
+						value = 0;
+					}
+				} else if (clazz == Long.class || clazz == long.class) {
+					try {
+						value = Long.parseLong(value.toString().trim());
+					} catch (NumberFormatException e) {
+						value = 0;
+					}
+				} else if (clazz == Float.class || clazz == float.class || clazz == Double.class
+						|| clazz == double.class) {
+					try {
+						value = Double.parseDouble(value.toString().trim());
+					} catch (NumberFormatException e) {
+						value = 0.0;
+					}
+				} else if (clazz == Boolean.class || clazz == boolean.class) {
+					value = strToBoolean(value.toString().trim());
+				} else {
+					value = null;
+				}
+			}
+			return (T) value;
+		}
+		return null;
+	}
+
+	/**
+	 * Get value (int type) from map. If not, return 0
+	 * 
+	 * @param props
+	 * @param key
+	 * @return
+	 */
+	public static int getInt(Map<?, ?> props, String key) {
+		Integer value = getValue(props, key, Integer.class);
+		return value == null ? 0 : value;
+	}
+
+	/**
+	 * Get value (String type) from map.
+	 * 
+	 * @param props
+	 * @param key
+	 * @return
+	 */
+	public static String getString(Map<?, ?> props, String key) {
+		Object value = props.get(key);
+		return value != null ? value.toString() : null;
+	}
+
+	/**
+	 * Converts a string to boolean value.
+	 * 
+	 * @param str
+	 *            String
+	 * @return boolean
+	 */
+	private static boolean strToBoolean(String str) {
+		if (str == null) {
+			return false;
+		}
+		if (str.equalsIgnoreCase("yes") || str.equalsIgnoreCase("true")
+				|| str.equalsIgnoreCase("y") || str.equalsIgnoreCase("t")
+				|| str.equalsIgnoreCase("1")) {
+			return true;
+		}
+		try {
+			if (Double.parseDouble(str) != 0.0) {
+				return true;
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return false;
+	}
+
+	/**
+	 * Generates an unique 64bit-keys (type String)
+	 * 
+	 * @return
+	 */
 	public static String generateId64() {
+		return Long.toHexString(generateLongId64());
+	}
+
+	/**
+	 * Generates an unique 64bit-keys (type long)
+	 * 
+	 * @return
+	 */
+	public static long generateLongId64() {
 		long timestamp = System.currentTimeMillis() - ORIGINAL_TIMESTAMP;
 		timestamp = timestamp & MASK_TIMESTAMP_32;
 		timestamp = timestamp << 32L;
 		long randomInt = rd.nextInt();
 		randomInt = randomInt & MASK_TIMESTAMP_32;
 		long result = timestamp | randomInt;
-		return Long.toHexString(result);
+		return result;
 	}
 
 }
