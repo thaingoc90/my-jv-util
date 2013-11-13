fe293554e68b3255d71f6ce4e44f3b4325020440
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-11-13 07:54:49 +0000

    - Commits voiceEffect project

---
+++ trunk/VoiceEffect/src/com/voiceeffect/activity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.voiceeffect.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	/**
+	 * Call when click 'record' button. 
+	 * @param view
+	 */
+	public void recordVoice(View view) {
+		
+	}
+
+}
