daa117820bb5a7fb1f1f5a86317fe172d96627ca
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2013-11-14 11:19:35 +0000

    - Record & Play

--- trunk/VoiceEffect/src/com/voiceeffect/activity/MainActivity.java
+++ trunk/VoiceEffect/src/com/voiceeffect/activity/MainActivity.java
@@ -1,69 +1,152 @@
 package com.voiceeffect.activity;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.view.Menu;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.media.MediaPlayer.OnCompletionListener;
 
 public class MainActivity extends Activity {
 
-	RecordButton recordBtn;
+	private static final String LOG_TAG = "VoiceEffect";
+	private static String mFileName = "";
+	Button recordBtn, playBtn;
+	boolean mStartRecording = true;
+	boolean mStartPlaying = true;
+	private MediaPlayer mPlayer = null;
+	private MediaRecorder mRecorder = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		recordBtn = (RecordButton) findViewById(R.id.btn_record);
-	}
+		initRecordingVariable();
+		initPlayingVariable();
 
-	class RecordButton extends Button {
+		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+		mFileName += "/TestAudio.mp3";
 
-		boolean mStartRecording = true;
-
-		OnClickListener clicker = new OnClickListener() {
+		recordBtn = (Button) findViewById(R.id.btn_record);
+		recordBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				if (mStartRecording) {
-					setText("StopRecord");
+					startRecording();
+					recordBtn.setText("StopRecord");
 				} else {
-					setText("StartRecord");
+					stopRecording();
+					recordBtn.setText("StartRecord");
 				}
 				mStartRecording = !mStartRecording;
 			}
-		};
+		});
 
-		public RecordButton(Context context) {
-			super(context);
-			setText("StartRecord");
-			setOnClickListener(clicker);
-		}
+		playBtn = (Button) findViewById(R.id.btn_normal_voice);
+		playBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mStartPlaying) {
+					startPlaying();
+					playBtn.setText("Stop");
+				} else {
+					stopPlaying();
+					playBtn.setText("Play");
+				}
+				mStartPlaying = !mStartPlaying;
 
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void initRecordingVariable() {
+		mStartRecording = true;
+		recordBtn.setText("StartRecord");
+	}
+
+	public void initPlayingVariable() {
+		mStartPlaying = true;
+		playBtn.setText("Play");
 	}
 
 	/**
-	 * Call when click 'record' button. Start recordActivity.
-	 * 
-	 * @param view
+	 * Start recording.
 	 */
-	public void recordVoice(View view) {
-		Intent recordIntent = new Intent(getApplicationContext(),
-				RecordVoiceActivity.class);
-		startActivityForResult(recordIntent, 0);
+	public void startRecording() {
+		Log.d(LOG_TAG, "start recording");
+		mRecorder = new MediaRecorder();
+		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+		mRecorder.setOutputFile(mFileName);
+		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+		try {
+			mRecorder.prepare();
+			mRecorder.start();
+		} catch (IOException e) {
+			Log.e(LOG_TAG, "Recording failed");
+		}
+	}
+
+	/**
+	 * Stop recording
+	 */
+	public void stopRecording() {
+		Log.d(LOG_TAG, "stop recording");
+		if (mRecorder != null) {
+			mRecorder.stop();
+			mRecorder.release();
+			mRecorder = null;
+		}
+	}
+
+	/**
+	 * Start playing
+	 */
+	public void startPlaying() {
+		mPlayer = new MediaPlayer();
+		Log.d(LOG_TAG, "start playing");
+		try {
+			File file = new File(mFileName);
+			if (!file.exists()) {
+				Log.i(LOG_TAG, "File not exist");
+			} else {
+				mPlayer.setDataSource(mFileName);
+				mPlayer.setOnCompletionListener(new OnCompletionListener() {
+					@Override
+					public void onCompletion(MediaPlayer mp) {
+						stopPlaying();
+						initPlayingVariable();
+					}
+				});
+				mPlayer.prepare();
+				mPlayer.start();
+			}
+		} catch (IOException e) {
+			Log.e(LOG_TAG, "Playing failed");
+			initPlayingVariable();
+		}
+	}
+
+	/**
+	 * Stop playing
+	 */
+	public void stopPlaying() {
+		Log.d(LOG_TAG, "stop playing");
+		if (mPlayer != null) {
+			mPlayer.release();
+			mPlayer = null;
+		}
 	}
 
 	public void startAlert(View view) {
