8b4501b85fc2132b53044729460f00a38115818c
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2014-06-12 02:52:59 +0000

    - edit NSWrapper

--- SoundEffects/jni/NSWrapper.cpp
+++ SoundEffects/jni/NSWrapper.cpp
@@ -1,24 +1,21 @@
 #include "NSWrapper.h"
 #include "Utils.h"
 #include "Log.h"
-
 #ifdef _FLOATING_POINT_
-
 #define W_WebRtcNs_Create WebRtcNs_Create
 #define W_WebRtcNs_Init WebRtcNs_Init
 #define W_WebRtcNs_Free WebRtcNs_Free
 #define W_WebRtcNs_Process WebRtcNs_Process
 #define W_WebRtcNs_set_policy WebRtcNs_set_policy
-
 #else
-
 #define W_WebRtcNs_Create WebRtcNsx_Create
 #define W_WebRtcNs_Init WebRtcNsx_Init
 #define W_WebRtcNs_Free WebRtcNsx_Free
 #define W_WebRtcNs_Process WebRtcNsx_Process
 #define W_WebRtcNs_set_policy WebRtcNsx_set_policy
-
 #endif
+W_NsHandle* NS_inst = NULL;
+unsigned NS_clock_rate = 0;
 
 /*
  * This function creates an instance & initializes a NS instance and has to be called before any other
@@ -34,21 +31,23 @@
  * Return value         :  0 - Ok
  *                        1 - Error
  */
-int NS_init(W_NsHandle** NS_inst, int mode) {
+int NS_init(int mode, unsigned clock_rate) {
 	Log::info("Init NS");
-	int res = W_WebRtcNs_Create(NS_inst);
+	int res = W_WebRtcNs_Create(&NS_inst);
 	if (res != STATUS_OK) {
 		return STATUS_FAIL;
 	}
-	res = W_WebRtcNs_Init((*NS_inst), SAMPLE_RATE);
+	res = W_WebRtcNs_Init(NS_inst, clock_rate);
 	if (res != STATUS_OK) {
 		return STATUS_FAIL;
 	}
 
+	NS_clock_rate = clock_rate;
+
 	if (mode < 0 || mode > 3) {
-		mode = 2;
+		mode = 3;
 	}
-	res = NS_set_policy((*NS_inst), mode);
+	res = NS_set_policy(NS_inst, mode);
 	if (res != STATUS_OK) {
 		return STATUS_FAIL;
 	}
@@ -66,12 +65,15 @@ int NS_init(W_NsHandle** NS_inst, int mode) {
  * Return value         :  0 - Ok
  *                         1 - Error
  */
-int NS_destroy(W_NsHandle* NS_inst) {
+int NS_destroy() {
 	Log::info("Destroy NS");
-	int res = W_WebRtcNs_Free(NS_inst);
-	NS_inst = NULL;
-	if (res != STATUS_OK) {
-		return STATUS_FAIL;
+	NS_clock_rate = 0;
+	if (NS_inst) {
+		int res = W_WebRtcNs_Free(NS_inst);
+		NS_inst = NULL;
+		if (res != STATUS_OK) {
+			return STATUS_FAIL;
+		}
 	}
 	return STATUS_OK;
 }
@@ -97,7 +99,6 @@ int NS_set_policy(W_NsHandle* NS_inst, int mode) {
 	return STATUS_OK;
 }
 
-
 /*
  * This functions does Noise Suppression for the inserted speech frame. The
  * input and output signals should always be 10ms (80 or 160 samples).
@@ -117,15 +118,34 @@ int NS_set_policy(W_NsHandle* NS_inst, int mode) {
  * Return value         :  0 - OK
  *                         1 - Error
  */
-int NS_Process(W_NsHandle* NS_inst, short* spframe, short* spframe_H,
-		short* outframe, short* outframe_H) {
-	if (SAMPLE_RATE == 32000 && (spframe_H == NULL || outframe_H == NULL)) {
+int NS_Process(short* rec_frm, unsigned samples_per_frame) {
+	int blockLen10ms = 160, i, status;
+	short tmp_frame[samples_per_frame], tmp_frame_H[samples_per_frame];
+
+	if (NS_clock_rate == 8000) {
+		blockLen10ms = 80;
+	} else if (NS_clock_rate == 16000 || NS_clock_rate == 32000) {
+		blockLen10ms = 160;
+	} else {
 		return STATUS_FAIL;
 	}
-	int res = W_WebRtcNs_Process(NS_inst, spframe, spframe_H, outframe,
-			outframe_H);
-	if (res != STATUS_OK) {
-		return STATUS_FAIL;
+
+	for (i = 0; i < samples_per_frame; i += blockLen10ms) {
+		if (NS_inst) {
+			if (NS_clock_rate == 32000) {
+				status = W_WebRtcNs_Process(NS_inst, (short*) (&rec_frm[i]),
+						(short*) (&rec_frm[i]), (short*) (&tmp_frame[i]),
+						(short*) (&tmp_frame_H[i]));
+			} else {
+				status = W_WebRtcNs_Process(NS_inst, (short*) (&rec_frm[i]),
+						NULL, (short*) (&tmp_frame[i]), NULL);
+			}
+			if (status != 0) {
+				Log::error("Error suppressing noise");
+				return STATUS_FAIL;
+			}
+		}
 	}
+	memcpy(rec_frm, tmp_frame, samples_per_frame * 2);
 	return STATUS_OK;
 }
