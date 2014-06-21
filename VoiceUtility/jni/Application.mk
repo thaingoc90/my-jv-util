066ff4441259a624b4989a31738922069c7d140d
thaingoc90@gmail.com <thaingoc90@gmail.com@36e1ad45-f9a8-a984-5229-7bbac57763c6>
2014-06-21 16:01:58 +0000

    Delete "error" Project VoiceUtility

--- VoiceUtility/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-# Build both ARMv5TE and ARMv7-A machine code.
-#APP_ABI := armeabi armeabi-v7a x86
-APP_ABI := armeabi-v7a x86
-APP_PLATFORM := android-9
-APP_STL := stlport_static
--- VoiceUtility/jni/GlobalConstant.h
+++
@@ -1,5 +0,0 @@
-#define _ANDROID_FLAG_
-//#define _CALLBACK_TO_DRAW_SOUNDWAVE_
-#define _AUDIO_SERVICE_USE_OPENSLES_
-//#define _USE_LAME_MP3_
-//#define _USE_OPENCORE_AMR_
